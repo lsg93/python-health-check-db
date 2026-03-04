909e06f3bc61f3602fab61ebf4e69319aaec85a2
Lawrence G <l.s.gwam@gmail.com>
2026-03-04 02:05:40 +0000

    more work

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from sqlalchemy-example!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ web/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(debug=True)
+
+
+def main():
+    print("fast api found")
+
+
+if __name__ == "__main__":
+    main()
